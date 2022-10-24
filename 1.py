4116b781d75d03852259c4eee6e322ac9deb2455
Andrii <andrburiy@gmail.com>
2022-10-24 13:28:18 +0300

    Lab2

---
+++ 1.py
@@ -0,0 +1,6 @@
+a = float(input("podaj długość boku a: "))
+b = float(input("podaj długość boku b: "))
+pole = a * b
+obwod = 2 * (a+b)
+#print('Pole: ',pole, '\nObwod = ', obwod)
+print(f'Pole:  {pole} \nObwod =  {obwod}')
